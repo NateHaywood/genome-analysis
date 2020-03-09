8a338ca937c991c5c59048305fc162368223bbee
Nate Haywood <nhaywood@umass.edu>
2020-03-09 13:52:04 -0400

    697 Lab2

---
+++ CommandLine.sh
@@ -0,0 +1,27 @@
+mkdir 697_lab2
+
+cd /Users/nate/Desktop/genome-analysis/697_lab2
+
+nano tempfile.txt
+
+ls -la
+
+cd ..
+
+cp index.Rmd temp2
+
+mv temp2 /Users/nate/Desktop/genome-analysis/697_lab2
+
+cd /Users/nate/Desktop/genome-analysis/697_lab2
+
+head -4 temp2 
+
+tail -4 temp2
+
+cat temp 2 >> tempfile
+
+rm -i temp2
+
+grep -i "Nathan Haywood" tempfile.txt
+
+w chmod tempfile u+g
